fd39cd434f22f88e6d183c00c97510385f5a1bb9
pranjalipongde09 <pranjalipongde9@gmail.com>
2025-09-03 16:51:56 +0530

    implementing blog post component with data

--- react-machine-coding/src/App.jsx
+++ react-machine-coding/src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import DarkAndLightTheme from "./components/DarkAndLightTheme";
 import DarkLightTheme from "./components/DarkLightTheme";
+import BlogPosts from "./components/BlogPosts";
 
 function App() {
   return (
     <>
       <DarkAndLightTheme />
       <DarkLightTheme />
+      <BlogPosts />
     </>
   );
 }
